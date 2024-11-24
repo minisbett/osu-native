481705629f4ea2f51c823d4473546a5f4f13fca6
minisbett <39670899+minisbett@users.noreply.github.com>
2024-11-24 01:17:15 +0100

    finalize mod support

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -5,10 +5,9 @@ using osu.Native.Bindings.Models.Osu;
 
 string osuFile = @"C:\Users\mini\Desktop\test.osu";
 
-
 OsuDifficultyCalculator calculator = new OsuDifficultyCalculator(new FileInfo(osuFile));
 
-Mod[] mods = [new Mod("DT", new() { ["speed_change"] = 2 })];
+Mod[] mods = [new Mod("DT", new() { ["speed_change"] = 2 }), new Mod("HD")];
 OsuDifficultyAttributes diffAttributes = calculator.CalculateDifficulty(mods);
 OsuPerformanceAttributes perfAttributes = calculator.CalculatePerformance(diffAttributes, new OsuScore(mods, 587));
 
