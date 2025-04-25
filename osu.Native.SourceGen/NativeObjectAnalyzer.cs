c56467e211ff8ce83e65fd28b00449626f67d3ac
minisbett <39670899+minisbett@users.noreply.github.com>
2025-04-25 15:46:14 +0200

    add code generator for Destroy, add analyzer for Id property

---
+++ osu.Native.SourceGen/NativeObjectAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace osu.Native.Analyzers;
+
+[Generator]
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class NativeObjectAnalyzer : DiagnosticAnalyzer
+{
+#pragma warning disable RS2008
+  private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor("OSU001", "Invalid NativeObject layout",
+    "The 'Id' property of a native object must be declared as the first property or field", "Usage", DiagnosticSeverity.Error, true,
+    "Ensures the 'Id' property is always declared first in the struct.");
+
+  public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [Rule];
+
+  public override void Initialize(AnalysisContext context)
+  {
+    context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+    context.EnableConcurrentExecution();
+    context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.StructDeclaration);
+  }
+
+  private void Analyze(SyntaxNodeAnalysisContext context)
+  {
+    var structDeclaration = (StructDeclarationSyntax)context.Node;
+    var structSymbol = context.SemanticModel.GetDeclaredSymbol(structDeclaration);
+    var iNativeObjectSymbol = context.Compilation.GetTypeByMetadataName("osu.Native.Objects.INativeObject`1");
+
+    if (!structSymbol.AllInterfaces.Any(x => x.OriginalDefinition.Equals(iNativeObjectSymbol, SymbolEqualityComparer.Default)))
+      return;
+
+    var idProperty = structSymbol.GetMembers("Id").FirstOrDefault();
+    var members = structDeclaration.Members.Where(x => x is PropertyDeclarationSyntax or FieldDeclarationSyntax && !x.Modifiers.Any(SyntaxKind.StaticKeyword));
+    if (idProperty is not null && members.First() != idProperty.DeclaringSyntaxReferences.First().GetSyntax())
+      context.ReportDiagnostic(Diagnostic.Create(Rule, idProperty.Locations[0]));
+  }
+}
---
+++ osu.Native.SourceGen/NativeObjectGenerator.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace osu.Native.Analyzers;
+
+[Generator]
+public class NativeObjectGenerator : IIncrementalGenerator
+{
+  public void Initialize(IncrementalGeneratorInitializationContext context)
+  {
+    var structs = context.SyntaxProvider.CreateSyntaxProvider(
+        predicate: static (s, _) => s is StructDeclarationSyntax,
+        transform: static (ctx, _) => (StructDeclarationSyntax)ctx.Node);
+
+
+    var source = context.CompilationProvider.Combine(structs.Collect());
+
+    context.RegisterSourceOutput(source, static (ctx, source) =>
+    {
+      var (compilation, structs) = source;
+      var iNativeObjectSymbol = compilation.GetTypeByMetadataName("osu.Native.Objects.INativeObject`1");
+
+      foreach (var structDeclaration in structs)
+      {
+        var model = compilation.GetSemanticModel(structDeclaration.SyntaxTree);
+        var structSymbol = model.GetDeclaredSymbol(structDeclaration);
+        if (!structSymbol.AllInterfaces.Any(x => x.OriginalDefinition.Equals(iNativeObjectSymbol, SymbolEqualityComparer.Default)))
+          continue;
+
+        string destroyMethod = GetDestroyMethodSource(structSymbol.Name);
+        string code = GetSource(structSymbol, [destroyMethod]);
+
+        code = CSharpSyntaxTree.ParseText(code).GetRoot().NormalizeWhitespace().ToFullString();
+        ctx.AddSource($"{structSymbol.Name}.g.cs", code);
+      }
+    });
+  }
+
+  private static string GetSource(INamedTypeSymbol @struct, IEnumerable<string> members)
+    => $$"""
+       using System.Runtime.InteropServices;
+       using System.Runtime.CompilerServices;
+
+       namespace {{@struct.ContainingNamespace}};
+
+       internal partial struct {{@struct.Name}}
+       {
+       {{string.Join("\n", members)}}
+       }
+       """;
+
+  private static string GetDestroyMethodSource(string structName)
+  {
+    string objectName = structName.StartsWith("Native") ? structName.Substring(6) : structName;
+
+    return $$"""
+           [UnmanagedCallersOnly(EntryPoint = "{{objectName}}_Destroy", CallConvs = [typeof(CallConvCdecl)])]
+           private static ErrorCode Destroy({{structName}} obj)
+           {
+             try
+             {
+               obj.Destroy();
+               return ErrorCode.Success;
+             }
+             catch (Exception ex)
+             {
+               return ErrorHandler.Handle(ex);
+             }
+           }
+           """;
+  }
+}
