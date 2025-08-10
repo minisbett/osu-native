9e0a1a001daffafdda5be1e2e4276f53e2bc6313
minisbett <39670899+minisbett@users.noreply.github.com>
2025-08-10 12:31:40 +0200

    ErrorCode refactoring

--- osu.Native/ErrorCode.cs
+++ osu.Native/ErrorCode.cs
@@ -1,7 +1,8 @@
 ﻿namespace osu.Native;
 
 /// <summary>
-/// Contains all error codes that can be returned from unmanaged functions.
+/// Contains all error codes that can be returned from unmanaged functions.<br/>
+/// <![CDATA[< 0 = special codes, 0 = success, > 0 = error]]>
 /// </summary>
 internal enum ErrorCode : sbyte
 {
@@ -16,24 +17,19 @@ internal enum ErrorCode : sbyte
   Success = 0,
 
   /// <summary>
-  /// Indicates that the managed object referenced by a native object was not found.
+  /// Indicates that the object referenced by a managed object handle was not found.
   /// </summary>
   ObjectNotFound = 1,
 
   /// <summary>
-  /// Indicates that the size of the provided buffer was too small to hold the requested data.
-  /// </summary>
-  BufferTooSmall = 2,
-
-  /// <summary>
   /// Indicates that the ruleset requested is not available (eg. not found).
   /// </summary>
-  RulesetUnavailable = 3,
+  RulesetUnavailable = 2,
 
   /// <summary>
   /// Indicates that the file in the scope of the operation was not found.
   /// </summary>
-  BeatmapFileNotFound = 4,
+  BeatmapFileNotFound = 3,
 
   /// <summary>
   /// Indicates an unspecific operation failure.
