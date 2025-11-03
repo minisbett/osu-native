f84c2b0fc706df6e43ac7099ab9986efc57788f0
minisbett <39670899+minisbett@users.noreply.github.com>
2025-11-03 23:25:10 +0100

    first unit tests

---
+++ osu.Native.Tests/ModTests.cs
@@ -0,0 +1,28 @@
+﻿using osu.Native.Objects;
+
+namespace osu.Native.Tests;
+
+[TestFixture]
+internal class ModTests
+{
+    [Test]
+    public void Mod_CreateFromAcronym_SuccessAndExpectedHandle()
+    {
+        ErrorCode errorCode = FFI.Mod_Create("DT", out NativeMod mod);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(errorCode, Is.EqualTo(ErrorCode.Success));
+            Assert.That(mod.Handle.Id, Is.EqualTo(1));
+        }
+    }
+
+    [Test]
+    public void Mod_SetSetting_Success()
+    {
+        FFI.Mod_Create("DT", out NativeMod mod);
+        ErrorCode errorCode = FFI.Mod_SetSetting(mod.Handle.Id, "speed_multiplier", 1.34);
+
+        Assert.That(errorCode, Is.EqualTo(ErrorCode.Success));
+    }
+}
---
+++ osu.Native.Tests/ModsCollectionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.Native.Objects;
+
+namespace osu.Native.Tests;
+
+[TestFixture]
+internal class ModsCollectionTests
+{
+    [Test]
+    public void ModsCollection_Create_SuccessAndExpectedHandle()
+    {
+        ErrorCode errorCode = FFI.ModsCollection_Create(out NativeModsCollection modsCollection);
+
+        Assert.That(errorCode, Is.EqualTo(ErrorCode.Success));
+    }
+
+    [Test]
+    public void ModsCollection_Add_Success()
+    {
+        FFI.Mod_Create("HD", out NativeMod nativeMod);
+        FFI.ModsCollection_Create(out NativeModsCollection modsCollection);
+        ErrorCode errorCode = FFI.ModsCollection_Add(modsCollection.Handle.Id, nativeMod.Handle.Id);
+
+        Assert.That(errorCode, Is.EqualTo(ErrorCode.Success));
+    }
+
+    [Test]
+    public void ModsCollection_AddAndRemove_Success()
+    {
+        FFI.Mod_Create("HD", out NativeMod nativeMod);
+        FFI.ModsCollection_Create(out NativeModsCollection modsCollection);
+        ErrorCode errorCode1 = FFI.ModsCollection_Add(modsCollection.Handle.Id, nativeMod.Handle.Id);
+        ErrorCode errorCode2 = FFI.ModsCollection_Remove(modsCollection.Handle.Id, nativeMod.Handle.Id);
+
+        Assert.That(errorCode1, Is.EqualTo(ErrorCode.Success));
+        Assert.That(errorCode2, Is.EqualTo(ErrorCode.Success));
+    }
+}
