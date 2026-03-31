7ea7e6b011af7292dfb3b9bbfae149e806f44238
minisbett <39670899+minisbett@users.noreply.github.com>
2026-03-31 12:26:07 +0200

    Add performance tests

---
+++ osu.Native.Tests/Objects/Performance/CatchPerformanceCalculatorTests.cs
@@ -0,0 +1,167 @@
+﻿using osu.Native.Objects;
+using osu.Native.Objects.Difficulty;
+using osu.Native.Objects.Performance;
+using osu.Native.Structures;
+using osu.Native.Structures.Difficulty;
+using osu.Native.Structures.Performance;
+
+namespace osu.Native.Tests.Objects.Performance;
+
+internal unsafe class CatchPerformanceCalculatorTests
+{
+    private NativeRuleset _nativeRuleset;
+    private NativeBeatmap _nativeBeatmap;
+    private NativeCatchPerformanceCalculator _nativePerformanceCalculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        fixed (NativeRuleset* ptr = &_nativeRuleset)
+            RulesetObject.CreateFromId(2, ptr);
+
+        _nativeBeatmap = TestUtils.CreateBeatmap("beatmaps/catch/Lite Show Magic (t+pazolite vs C-Show) - Crack Traxxxx (Fatfan Kolek) [Spec's Hi-Speed Overdose].osu");
+
+        fixed (NativeCatchPerformanceCalculator* ptr = &_nativePerformanceCalculator)
+            CatchPerformanceCalculatorObject.Create(ptr);
+    }
+
+    [Test]
+    public void Create_Success()
+    {
+        NativeOsuPerformanceCalculator nativePerformanceCalculator;
+        ErrorCode errorCode = OsuPerformanceCalculatorObject.Create(&nativePerformanceCalculator);
+
+        Assert.That(errorCode, Is.EqualTo(ErrorCode.Success));
+    }
+
+    [TestCaseSource(nameof(GetTestCases))]
+    public void Calculate_Success(string beatmapFilename, string? mods, NativeScoreInfo scoreInfo, NativeCatchPerformanceAttributes expectedAttributes)
+    {
+        NativeBeatmap nativeBeatmap = TestUtils.CreateBeatmap(beatmapFilename);
+        NativeModsCollection nativeModsCollection = TestUtils.CreateNativeModsCollection(mods);
+
+        scoreInfo.RulesetHandle = _nativeRuleset.Handle;
+        scoreInfo.BeatmapHandle = nativeBeatmap.Handle;
+        scoreInfo.ModsHandle = nativeModsCollection.Handle;
+
+        NativeCatchDifficultyCalculator nativeDifficultyCalculator;
+        CatchDifficultyCalculatorObject.Create(_nativeRuleset.Handle, nativeBeatmap.Handle, &nativeDifficultyCalculator);
+
+        NativeCatchDifficultyAttributes nativeDifficultyAttributes;
+        CatchDifficultyCalculatorObject.Calculate(nativeDifficultyCalculator.Handle, nativeModsCollection.Handle, &nativeDifficultyAttributes);
+
+        NativeCatchPerformanceAttributes nativeAttributes;
+        ErrorCode errorCode = CatchPerformanceCalculatorObject.Calculate(
+            _nativePerformanceCalculator.Handle, scoreInfo, nativeDifficultyAttributes, &nativeAttributes);
+
+        Assert.That(errorCode, Is.EqualTo(ErrorCode.Success));
+        Assert.That(nativeAttributes, Is.EqualTo(expectedAttributes));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCases()
+    {
+        yield return new(
+            "beatmaps/osu/Kenji Ninuma - DISCOPRINCE (peppy) [Normal].osu",
+            null,
+            new NativeScoreInfo()
+            {
+                MaxCombo = 310,
+                Accuracy = 1,
+                LegacyTotalScore = null,
+                CountMiss = 0,
+                CountMeh = 0,
+                CountOk = 0,
+                CountGood = 0,
+                CountGreat = 235,
+                CountPerfect = 0,
+                CountSmallTickMiss = 0,
+                CountSmallTickHit = 360,
+                CountLargeTickMiss = 0,
+                CountLargeTickHit = 75,
+                CountSliderTailHit = 0
+            },
+            new NativeCatchPerformanceAttributes(new()
+            {
+                Total = 18.924552117524012
+            })
+        );
+
+        yield return new(
+            "beatmaps/catch/Lite Show Magic (t+pazolite vs C-Show) - Crack Traxxxx (Fatfan Kolek) [Spec's Hi-Speed Overdose].osu",
+            "DTFL",
+            new NativeScoreInfo()
+            {
+                MaxCombo = 519,
+                Accuracy = 0.8962892483349191,
+                LegacyTotalScore = null,
+                CountMiss = 17,
+                CountMeh = 0,
+                CountOk = 0,
+                CountGood = 0,
+                CountGreat = 892,
+                CountPerfect = 0,
+                CountSmallTickMiss = 92,
+                CountSmallTickHit = 15,
+                CountLargeTickMiss = 0,
+                CountLargeTickHit = 35,
+                CountSliderTailHit = 0
+            },
+            new NativeCatchPerformanceAttributes(new()
+            {
+                Total = 306.481176897223
+            })
+        );
+
+        yield return new(
+            "beatmaps/catch/Hanatan - Airman ga Taosenai (SOUND HOLIC Ver.) (Natsu) [Zero's Overdose].osu",
+            "FFEZ",
+            new NativeScoreInfo()
+            {
+                MaxCombo = 924,
+                Accuracy = 1,
+                LegacyTotalScore = null,
+                CountMiss = 2,
+                CountMeh = 0,
+                CountOk = 0,
+                CountGood = 0,
+                CountGreat = 979,
+                CountPerfect = 0,
+                CountSmallTickMiss = -2,
+                CountSmallTickHit = 244,
+                CountLargeTickMiss = 0,
+                CountLargeTickHit = 2,
+                CountSliderTailHit = 0
+            },
+            new NativeCatchPerformanceAttributes(new()
+            {
+                Total = 250.24123058175263
+            })
+        );
+
+        yield return new(
+            "beatmaps/catch/Icon For Hire - Make a Move (Speed Up Ver.) (Sotarks) [Ascendance's Overdose].osu",
+            "MF",
+            new NativeScoreInfo()
+            {
+                MaxCombo = 204,
+                Accuracy = 0.7258064516129032,
+                LegacyTotalScore = null,
+                CountMiss = 3,
+                CountMeh = 0,
+                CountOk = 0,
+                CountGood = 0,
+                CountGreat = 242,
+                CountPerfect = 0,
+                CountSmallTickMiss = 99,
+                CountSmallTickHit = 11,
+                CountLargeTickMiss = 0,
+                CountLargeTickHit = 17,
+                CountSliderTailHit = 0
+            },
+            new NativeCatchPerformanceAttributes(new()
+            {
+                Total = 31.727829984154145
+            })
+        );
+    }
+}
---
+++ osu.Native.Tests/Objects/Performance/TaikoPerformanceCalculatorTests.cs
@@ -0,0 +1,179 @@
+﻿using osu.Native.Objects;
+using osu.Native.Objects.Difficulty;
+using osu.Native.Objects.Performance;
+using osu.Native.Structures;
+using osu.Native.Structures.Difficulty;
+using osu.Native.Structures.Performance;
+
+namespace osu.Native.Tests.Objects.Performance;
+
+internal unsafe class TaikoPerformanceCalculatorTests
+{
+    private NativeRuleset _nativeRuleset;
+    private NativeBeatmap _nativeBeatmap;
+    private NativeTaikoPerformanceCalculator _nativePerformanceCalculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        fixed (NativeRuleset* ptr = &_nativeRuleset)
+            RulesetObject.CreateFromId(1, ptr);
+
+        _nativeBeatmap = TestUtils.CreateBeatmap("beatmaps/taiko/Nanamori-chu  Goraku-bu - Happy Time wa Owaranai (eiri-) [Oni].osu");
+
+        fixed (NativeTaikoPerformanceCalculator* ptr = &_nativePerformanceCalculator)
+            TaikoPerformanceCalculatorObject.Create(ptr);
+    }
+
+    [Test]
+    public void Create_Success()
+    {
+        NativeOsuPerformanceCalculator nativePerformanceCalculator;
+        ErrorCode errorCode = OsuPerformanceCalculatorObject.Create(&nativePerformanceCalculator);
+
+        Assert.That(errorCode, Is.EqualTo(ErrorCode.Success));
+    }
+
+    [TestCaseSource(nameof(GetTestCases))]
+    public void Calculate_Success(string beatmapFilename, string? mods, NativeScoreInfo scoreInfo, NativeTaikoPerformanceAttributes expectedAttributes)
+    {
+        NativeBeatmap nativeBeatmap = TestUtils.CreateBeatmap(beatmapFilename);
+        NativeModsCollection nativeModsCollection = TestUtils.CreateNativeModsCollection(mods);
+
+        scoreInfo.RulesetHandle = _nativeRuleset.Handle;
+        scoreInfo.BeatmapHandle = nativeBeatmap.Handle;
+        scoreInfo.ModsHandle = nativeModsCollection.Handle;
+
+        NativeTaikoDifficultyCalculator nativeDifficultyCalculator;
+        TaikoDifficultyCalculatorObject.Create(_nativeRuleset.Handle, nativeBeatmap.Handle, &nativeDifficultyCalculator);
+
+        NativeTaikoDifficultyAttributes nativeDifficultyAttributes;
+        TaikoDifficultyCalculatorObject.Calculate(nativeDifficultyCalculator.Handle, nativeModsCollection.Handle, &nativeDifficultyAttributes);
+
+        NativeTaikoPerformanceAttributes nativeAttributes;
+        ErrorCode errorCode = TaikoPerformanceCalculatorObject.Calculate(
+            _nativePerformanceCalculator.Handle, scoreInfo, nativeDifficultyAttributes, &nativeAttributes);
+
+        Assert.That(errorCode, Is.EqualTo(ErrorCode.Success));
+        Assert.That(nativeAttributes, Is.EqualTo(expectedAttributes));
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCases()
+    {
+        yield return new(
+            "beatmaps/osu/Kenji Ninuma - DISCOPRINCE (peppy) [Normal].osu",
+            null,
+            new NativeScoreInfo()
+            {
+                MaxCombo = 208,
+                Accuracy = 1,
+                LegacyTotalScore = null,
+                CountMiss = 0,
+                CountMeh = 0,
+                CountOk = 0,
+                CountGood = 0,
+                CountGreat = 208,
+                CountPerfect = 0,
+                CountSmallTickMiss = 0,
+                CountSmallTickHit = 0,
+                CountLargeTickMiss = 0,
+                CountLargeTickHit = 0,
+                CountSliderTailHit = 0
+            },
+            new NativeTaikoPerformanceAttributes(new()
+            {
+                Total = 104.51117530615562,
+                Difficulty = 2.156834874920385,
+                Accuracy = 102.35434043123524,
+                EstimatedUnstableRate = 140.90673313955597
+            })
+        );
+
+        yield return new(
+            "beatmaps/taiko/Nanamori-chu  Goraku-bu - Happy Time wa Owaranai (eiri-) [Oni].osu",
+            "DT",
+            new NativeScoreInfo()
+            {
+                MaxCombo = 697,
+                Accuracy = 0.9870801033591732,
+                LegacyTotalScore = null,
+                CountMiss = 4,
+                CountMeh = 0,
+                CountOk = 12,
+                CountGood = 0,
+                CountGreat = 758,
+                CountPerfect = 0,
+                CountSmallTickMiss = 0,
+                CountSmallTickHit = 0,
+                CountLargeTickMiss = 0,
+                CountLargeTickHit = 0,
+                CountSliderTailHit = 0
+            },
+            new NativeTaikoPerformanceAttributes(new()
+            {
+                Total = 412.4392958566681,
+                Difficulty = 233.62531746709058,
+                Accuracy = 178.81397838957753,
+                EstimatedUnstableRate = 100.34576232908552
+            })
+        );
+
+        yield return new(
+            "beatmaps/taiko/AliA - Kakurenbo (Santi199) [From Here].osu",
+            "HDEZ",
+            new NativeScoreInfo()
+            {
+                MaxCombo = 214,
+                Accuracy = 0.9642121524201854,
+                LegacyTotalScore = null,
+                CountMiss = 37,
+                CountMeh = 0,
+                CountOk = 65,
+                CountGood = 0,
+                CountGreat = 1840,
+                CountPerfect = 0,
+                CountSmallTickMiss = 0,
+                CountSmallTickHit = 0,
+                CountLargeTickMiss = 0,
+                CountLargeTickHit = 0,
+                CountSliderTailHit = 0
+            },
+            new NativeTaikoPerformanceAttributes(new()
+            {
+                Total = 146.25134755466507,
+                Difficulty = 98.85314432466512,
+                Accuracy = 47.39820322999994,
+                EstimatedUnstableRate = 220.02760013950285
+            })
+        );
+
+        yield return new(
+            "beatmaps/taiko/The Quick Brown Fox - The Big Black (Blue Dragon) [Ono's Taiko Oni].osu",
+            "FL",
+            new NativeScoreInfo()
+            {
+                MaxCombo = 758,
+                Accuracy = 0.9883843717001056,
+                LegacyTotalScore = null,
+                CountMiss = 11,
+                CountMeh = 0,
+                CountOk = 0,
+                CountGood = 0,
+                CountGreat = 936,
+                CountPerfect = 0,
+                CountSmallTickMiss = 0,
+                CountSmallTickHit = 0,
+                CountLargeTickMiss = 0,
+                CountLargeTickHit = 0,
+                CountSliderTailHit = 0
+            },
+            new NativeTaikoPerformanceAttributes(new()
+            {
+                Total = 269.2142303936673,
+                Difficulty = 158.01180448187534,
+                Accuracy = 111.20242591179195,
+                EstimatedUnstableRate = 138.50375768093897
+            })
+        );
+    }
+}
